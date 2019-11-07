dfd0a7d6e1736459ef57e23c7e0bde5ae1ca093c
Tara Z. Manicsic <tzmanics@gmail.com>
2019-11-07 00:00:06 -0500

    lint index.ts and start schema options

--- src/netlify-ng-schematics/index.ts
+++ src/netlify-ng-schematics/index.ts
@@ -1,8 +1,5 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
 
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function netlifyNgSchematics(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     return tree;
