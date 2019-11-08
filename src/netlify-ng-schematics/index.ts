1ac78d33e5382fefa408844ce0639de8183fc624
Tara Z. Manicsic <tzmanics@gmail.com>
2019-11-08 14:46:14 -0500

    restructures for library schematic

--- src/netlify-ng-schematics/index.ts
+++
@@ -1,7 +0,0 @@
-import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
-
-export function netlifyNgSchematics(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
-}
